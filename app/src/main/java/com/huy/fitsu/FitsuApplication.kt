3f66c33a74654e4011d87562e49044d3ab54fbd8
Huy Bui <huythdnbkltv@gmail.com>
2020-03-29 15:19:56 +0300

    Initialize Dagger dependency graph with application context

--- app/src/main/java/com/huy/fitsu/FitsuApplication.kt
+++ app/src/main/java/com/huy/fitsu/FitsuApplication.kt
@@ -1,13 +1,23 @@
 package com.huy.fitsu
 
 import android.app.Application
+import com.huy.fitsu.di.AppComponent
+import com.huy.fitsu.di.DaggerAppComponent
 import timber.log.Timber
 
 class FitsuApplication: Application() {
 
+    val appComponent: AppComponent by lazy {
+        initAppComponent()
+    }
+
     override fun onCreate() {
         super.onCreate()
         if (BuildConfig.DEBUG) Timber.plant(Timber.DebugTree())
+    }
+
+    private fun initAppComponent(): AppComponent {
+        return DaggerAppComponent.factory().create(applicationContext)
     }
 
 }
---
+++ app/src/main/java/com/huy/fitsu/di/AppComponent.kt
@@ -0,0 +1,17 @@
+package com.huy.fitsu.di
+
+import android.content.Context
+import dagger.BindsInstance
+import dagger.Component
+import javax.inject.Singleton
+
+@Singleton
+@Component
+interface AppComponent {
+
+    @Component.Factory
+    interface Factory {
+        fun create(@BindsInstance context: Context): AppComponent
+    }
+
+}
