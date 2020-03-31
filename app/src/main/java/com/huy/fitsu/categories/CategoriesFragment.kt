6ec085dcf49878ee7b79bbaa88786df58265644a
Huy Bui <huythdnbkltv@gmail.com>
2020-03-31 15:00:50 +0300

    Inflate CategoriesFragment

--- app/src/main/java/com/huy/fitsu/categories/CategoriesFragment.kt
+++ app/src/main/java/com/huy/fitsu/categories/CategoriesFragment.kt
@@ -1,10 +1,15 @@
 package com.huy.fitsu.categories
 
 import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
 import com.huy.fitsu.FitsuApplication
+import com.huy.fitsu.databinding.CategoriesFragBinding
 import javax.inject.Inject
 
 class CategoriesFragment: Fragment() {
@@ -14,12 +19,23 @@ class CategoriesFragment: Fragment() {
 
     private val viewModel by viewModels<CategoriesViewModel> { viewModelFactory }
 
+    private lateinit var binding: CategoriesFragBinding
+
     override fun onAttach(context: Context) {
         super.onAttach(context)
         (requireActivity().application as FitsuApplication).appComponent
             .categoriesComponent()
             .create()
             .inject(this)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = CategoriesFragBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
 }
