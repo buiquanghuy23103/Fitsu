f87add763e88d589517691e9aa16823c6a634e4a
Huy Bui <huythdnbkltv@gmail.com>
2020-03-31 11:33:41 +0300

    Add CategoriesViewModel

---
+++ app/src/main/java/com/huy/fitsu/categories/CategoriesViewModel.kt
@@ -0,0 +1,20 @@
+package com.huy.fitsu.categories
+
+import androidx.lifecycle.ViewModel
+import com.huy.fitsu.data.model.Category
+import com.huy.fitsu.data.repository.CategoryRepository
+
+class CategoriesViewModel(
+    private val repository: CategoryRepository
+): ViewModel() {
+
+    init {
+        val food = Category(title = "Food")
+        val houseRent = Category(title = "House rent")
+        repository.addCategory(food)
+        repository.addCategory(houseRent)
+    }
+
+    fun getAllCategories() = repository.getAllCategories()
+
+}
