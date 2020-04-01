5912f89111a787f9d680310f60ba40bfed26ead7
Huy Bui <huythdnbkltv@gmail.com>
2020-04-01 17:23:42 +0300

    Fix injection error on FitsuSchedulerImpl

--- app/src/main/java/com/huy/fitsu/scheduler/FitsuSchedulerImpl.kt
+++ app/src/main/java/com/huy/fitsu/scheduler/FitsuSchedulerImpl.kt
@@ -1,8 +1,9 @@
 package com.huy.fitsu.scheduler
 
 import io.reactivex.schedulers.Schedulers
+import javax.inject.Inject
 
-class FitsuSchedulerImpl: FitsuScheduler {
+class FitsuSchedulerImpl @Inject constructor(): FitsuScheduler {
 
     override fun io() = Schedulers.io()
 
