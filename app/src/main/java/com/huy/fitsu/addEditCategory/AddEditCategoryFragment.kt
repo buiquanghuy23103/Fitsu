c07aaf99ec57e5ab576c847b3d6353c45f6873d0
Huy Bui <huythdnbkltv@gmail.com>
2020-04-01 21:39:09 +0300

    Inflate category title edit text

--- app/src/main/java/com/huy/fitsu/addEditCategory/AddEditCategoryFragment.kt
+++ app/src/main/java/com/huy/fitsu/addEditCategory/AddEditCategoryFragment.kt
@@ -7,7 +7,9 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.navArgs
 import com.huy.fitsu.FitsuApplication
 import com.huy.fitsu.databinding.AddEditCategoryFragBinding
 import javax.inject.Inject
@@ -18,6 +20,9 @@ class AddEditCategoryFragment: Fragment() {
     lateinit var viewModelFactory: ViewModelProvider.Factory
 
     private val viewModel by viewModels<AddEditCategoryViewModel> { viewModelFactory }
+
+    private val args: AddEditCategoryFragmentArgs by navArgs()
+
     private lateinit var binding: AddEditCategoryFragBinding
 
     override fun onAttach(context: Context) {
@@ -36,6 +41,25 @@ class AddEditCategoryFragment: Fragment() {
     ): View? {
         binding = AddEditCategoryFragBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        binding.lifecycleOwner = this.viewLifecycleOwner
+        setCategoryId()
+        loadCategoryInfo()
+    }
+
+    private fun setCategoryId() {
+        val categoryId = args.categoryId
+        viewModel.setCategoryId(categoryId)
+    }
+
+    private fun loadCategoryInfo() {
+        viewModel.getCategory().observe(viewLifecycleOwner, Observer {
+            it?.let { category -> binding.category = category }
+        })
     }
 
 }
