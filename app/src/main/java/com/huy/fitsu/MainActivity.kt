5fa2bfc63962bbacb5a4234bdd99437e1372a342
Huy Bui <huythdnbkltv@gmail.com>
2020-04-02 21:51:20 +0300

    Fix toolbar

--- app/src/main/java/com/huy/fitsu/MainActivity.kt
+++ app/src/main/java/com/huy/fitsu/MainActivity.kt
@@ -2,11 +2,36 @@ package com.huy.fitsu
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setSupportActionBar(toolbar)
+
+        setupBottomNavigation()
+    }
+
+    private fun setupBottomNavigation() {
+        val navController = findNavController(R.id.nav_host_fragment)
+        appBarConfiguration = AppBarConfiguration(setOf(
+            R.id.categoriesFragment))
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        bottom_navigation.setupWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return findNavController(R.id.nav_host_fragment).navigateUp(appBarConfiguration)
+                || super.onSupportNavigateUp()
     }
 }
