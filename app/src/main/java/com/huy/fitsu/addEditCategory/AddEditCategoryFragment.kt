c284adbb9d9fd02aaad631aefdf2c30966aca05b
Huy Bui <huythdnbkltv@gmail.com>
2020-04-02 09:23:21 +0300

    Add drop-down menu

--- app/src/main/java/com/huy/fitsu/addEditCategory/AddEditCategoryFragment.kt
+++ app/src/main/java/com/huy/fitsu/addEditCategory/AddEditCategoryFragment.kt
@@ -5,12 +5,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.navArgs
 import com.huy.fitsu.FitsuApplication
+import com.huy.fitsu.R
+import com.huy.fitsu.data.model.BudgetDuration
 import com.huy.fitsu.databinding.AddEditCategoryFragBinding
 import javax.inject.Inject
 
@@ -60,6 +63,17 @@ class AddEditCategoryFragment: Fragment() {
         viewModel.getCategory().observe(viewLifecycleOwner, Observer {
             it?.let { category -> binding.category = category }
         })
+
+        setupDropDownMenu()
+    }
+
+    private fun setupDropDownMenu() {
+        val adapter = ArrayAdapter(
+            requireContext(),
+            R.layout.dropdown_menu_popup_item,
+            BudgetDuration.values()
+        )
+        binding.categoryBudgetDurationEditText.setAdapter(adapter)
     }
 
 }
