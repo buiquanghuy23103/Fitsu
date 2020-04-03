692a27eacf4f12f7c372bce5bdb54aa3a0580a55
Huy Bui <huythdnbkltv@gmail.com>
2020-04-03 16:13:28 +0300

    Hide bottom navigation when editing a category

--- app/src/main/java/com/huy/fitsu/MainActivity.kt
+++ app/src/main/java/com/huy/fitsu/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.huy.fitsu
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
@@ -12,6 +14,7 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,14 +23,38 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
 
         setupBottomNavigation()
+
+        showHideBottomNavigation()
     }
 
     private fun setupBottomNavigation() {
-        val navController = findNavController(R.id.nav_host_fragment)
+        navController = findNavController(R.id.nav_host_fragment)
         appBarConfiguration = AppBarConfiguration(setOf(
             R.id.categoriesFragment))
         setupActionBarWithNavController(navController, appBarConfiguration)
         bottom_navigation.setupWithNavController(navController)
+    }
+
+    private fun showHideBottomNavigation() {
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.categoriesFragment -> showBottomNavigation()
+                R.id.addEditCategoryFragment -> hideBottomNavigation()
+            }
+        }
+    }
+
+    private fun showBottomNavigation() {
+        controlBottomNavigationVisibility(View.VISIBLE)
+    }
+
+    private fun hideBottomNavigation() {
+        controlBottomNavigationVisibility(View.GONE)
+    }
+
+    private fun controlBottomNavigationVisibility(visibility: Int) {
+        bottom_bar.visibility = visibility
+        add_transactions_fab.visibility = visibility
     }
 
     override fun onSupportNavigateUp(): Boolean {
