b6ea678c7ff0d4f107e3456b90aa33bd88e9054f
Huy Bui <huythdnbkltv@gmail.com>
2020-04-04 21:45:30 +0300

    Remove redundant methods

--- app/src/main/java/com/huy/fitsu/data/local/CategoryDao.kt
+++ app/src/main/java/com/huy/fitsu/data/local/CategoryDao.kt
@@ -3,7 +3,6 @@ package com.huy.fitsu.data.local
 import androidx.lifecycle.LiveData
 import androidx.room.*
 import com.huy.fitsu.data.model.Category
-import io.reactivex.Completable
 
 @Dao
 interface CategoryDao {
@@ -13,19 +12,6 @@ interface CategoryDao {
 
     @Query("SELECT * FROM categories")
     fun getAll(): LiveData<List<Category>>
-
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
-    fun insert(category: Category): Completable
-
-    @Update
-    fun update(category: Category): Completable
-
-
-    @Query("SELECT * FROM categories WHERE id = :id")
-    suspend fun getCategoryById(id: String): Category
-
-    @Query("SELECT * FROM categories")
-    suspend fun getAllCategories(): List<Category>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertNewCategory(category: Category)
