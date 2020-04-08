15ece1eb33d5a54d85f94dc98557ae8303399182
Huy Bui <huythdnbkltv@gmail.com>
2020-04-08 11:53:14 +0300

    Display DashboardFragment

---
+++ app/src/main/java/com/huy/fitsu/dashboard/DashboardFragment.kt
@@ -0,0 +1,41 @@
+package com.huy.fitsu.dashboard
+
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.ViewModelProvider
+import com.huy.fitsu.FitsuApplication
+import com.huy.fitsu.databinding.DashboardFragBinding
+import javax.inject.Inject
+
+class DashboardFragment: Fragment() {
+
+    @Inject
+    lateinit var viewModelFactory: ViewModelProvider.Factory
+
+    private val viewModel by viewModels<TransactionsViewModel> { viewModelFactory }
+
+    private lateinit var binding: DashboardFragBinding
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        (requireActivity().application as FitsuApplication).appComponent
+            .dashboardComponent()
+            .create()
+            .inject(this)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DashboardFragBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+}
