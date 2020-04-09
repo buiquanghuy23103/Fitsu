05af16bee412cf9266222eabaa8afd7ad9217ca7
Huy Bui <huythdnbkltv@gmail.com>
2020-04-09 12:28:19 +0300

    Setup Dagger for instrumented tests

---
+++ app/src/androidTest/java/com/huy/fitsu/di/TestAppComponent.kt
@@ -0,0 +1,31 @@
+package com.huy.fitsu.di
+
+import android.content.Context
+import com.huy.fitsu.data.manager.DataSourceModule
+import com.huy.fitsu.data.repository.RepositoryModule
+import com.huy.fitsu.scheduler.SchedulerModule
+import dagger.BindsInstance
+import dagger.Component
+import javax.inject.Singleton
+
+@Singleton
+@Component(
+    modules = [
+        DatabaseModule::class,
+        DaoModule::class,
+        SubComponentModule::class,
+        ViewModelBuilderModule::class,
+        RepositoryModule::class,
+        SchedulerModule::class,
+        DispatcherModule::class,
+        DataSourceModule::class
+    ]
+)
+interface TestAppComponent: AppComponent {
+
+    @Component.Factory
+    interface Factory {
+        fun create(@BindsInstance context: Context): AppComponent
+    }
+
+}
