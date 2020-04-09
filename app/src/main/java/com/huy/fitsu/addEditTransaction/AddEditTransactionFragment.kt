67f9221679f37598afd4ec0be1d0457c358920ba
Huy Bui <huythdnbkltv@gmail.com>
2020-04-09 09:21:21 +0300

    Get transaction's ID from navigation arguments

--- app/src/main/java/com/huy/fitsu/addEditTransaction/AddEditTransactionFragment.kt
+++ app/src/main/java/com/huy/fitsu/addEditTransaction/AddEditTransactionFragment.kt
@@ -6,12 +6,25 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.navArgs
 import com.huy.fitsu.FitsuApplication
 import com.huy.fitsu.databinding.AddEditTransactionFragBinding
+import javax.inject.Inject
 
 class AddEditTransactionFragment: Fragment() {
 
+    @Inject
+    lateinit var viewModelFactory: ViewModelProvider.Factory
+
     private lateinit var binding: AddEditTransactionFragBinding
+
+    private val viewModel by viewModels<AddEditTransactionViewModel> { viewModelFactory }
+
+    private val navArgs : AddEditTransactionFragmentArgs by navArgs()
+
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
@@ -28,6 +41,19 @@ class AddEditTransactionFragment: Fragment() {
     ): View? {
         binding = AddEditTransactionFragBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        viewModel.withTransactionId(navArgs.transactionId)
+        binding.lifecycleOwner = viewLifecycleOwner
+
+        viewModel.transaction.observe(viewLifecycleOwner, Observer {
+            it?.let { transaction ->
+                binding.transaction = transaction
+            }
+        })
     }
 
 }
--- app/src/main/java/com/huy/fitsu/addEditTransaction/AddEditTransactionViewModel.kt
+++ app/src/main/java/com/huy/fitsu/addEditTransaction/AddEditTransactionViewModel.kt
@@ -1,8 +1,22 @@
 package com.huy.fitsu.addEditTransaction
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import com.huy.fitsu.data.model.Transaction
+import com.huy.fitsu.data.repository.TransactionRepository
+import javax.inject.Inject
 
-class AddEditTransactionViewModel(
-
+class AddEditTransactionViewModel @Inject constructor(
+    private val transactionRepository: TransactionRepository
 ) : ViewModel() {
+
+    private var transactionId = ""
+
+    fun withTransactionId(id: String) {
+        this.transactionId = id
+    }
+
+    val transaction : LiveData<Transaction>
+        get() = transactionRepository.getTransaction(transactionId)
+
 }
