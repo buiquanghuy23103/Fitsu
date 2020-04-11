49aaac45edc455c6777a8fef641a9f2467d8857e
Huy Bui <huythdnbkltv@gmail.com>
2020-04-11 15:33:31 +0300

    Refactor navigation drawer

--- app/src/main/java/com/huy/fitsu/MainActivity.kt
+++ app/src/main/java/com/huy/fitsu/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.huy.fitsu
 
 import android.os.Bundle
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
@@ -9,7 +8,6 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.main_act.*
 
 class MainActivity : AppCompatActivity() {
@@ -35,42 +33,12 @@ class MainActivity : AppCompatActivity() {
     private fun setupNavigation() {
         navController = findNavController(R.id.nav_host_fragment)
         appBarConfiguration = AppBarConfiguration.Builder(
-            R.id.categoriesFragment
+            R.id.categoriesFragment, R.id.dashboardFragment
         )
             .setOpenableLayout(drawer_layout)
             .build()
         setupActionBarWithNavController(navController, appBarConfiguration)
         nav_view.setupWithNavController(navController)
-    }
-
-    private fun setupBottomNavigation() {
-        navController = findNavController(R.id.nav_host_fragment)
-        appBarConfiguration = AppBarConfiguration(setOf(
-            R.id.categoriesFragment))
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        bottom_navigation.setupWithNavController(navController)
-    }
-
-    private fun showHideBottomNavigation() {
-        navController.addOnDestinationChangedListener { _, destination, _ ->
-            when (destination.id) {
-                R.id.categoriesFragment -> showBottomNavigation()
-                R.id.addEditCategoryFragment -> hideBottomNavigation()
-            }
-        }
-    }
-
-    private fun showBottomNavigation() {
-        controlBottomNavigationVisibility(View.VISIBLE)
-    }
-
-    private fun hideBottomNavigation() {
-        controlBottomNavigationVisibility(View.GONE)
-    }
-
-    private fun controlBottomNavigationVisibility(visibility: Int) {
-        bottom_bar.visibility = visibility
-        add_transactions_fab.visibility = visibility
     }
 
     override fun onSupportNavigateUp(): Boolean {
