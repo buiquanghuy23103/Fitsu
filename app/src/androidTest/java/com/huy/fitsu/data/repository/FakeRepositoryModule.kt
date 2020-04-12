baf6d5eca97777a37f6110ca0aacf79fca661ac2
Huy Bui <huythdnbkltv@gmail.com>
2020-04-12 13:13:24 +0300

    repositories should be singletons

--- app/src/androidTest/java/com/huy/fitsu/data/repository/FakeRepositoryModule.kt
+++ app/src/androidTest/java/com/huy/fitsu/data/repository/FakeRepositoryModule.kt
@@ -2,14 +2,17 @@ package com.huy.fitsu.data.repository
 
 import dagger.Binds
 import dagger.Module
+import javax.inject.Singleton
 
 @Module
 abstract class FakeRepositoryModule {
 
 
+    @Singleton
     @Binds
     abstract fun categoriesRepository(repository: FakeCategoryRepository): CategoryRepository
 
+    @Singleton
     @Binds
     abstract fun transactionsRepository(repo: TransactionRepositoryImpl): TransactionRepository
 
--- app/src/main/java/com/huy/fitsu/data/repository/RepositoryModule.kt
+++ app/src/main/java/com/huy/fitsu/data/repository/RepositoryModule.kt
@@ -2,13 +2,16 @@ package com.huy.fitsu.data.repository
 
 import dagger.Binds
 import dagger.Module
+import javax.inject.Singleton
 
 @Module
 abstract class RepositoryModule {
 
+    @Singleton
     @Binds
     abstract fun categoriesRepository(repository: CategoryRepositoryImpl): CategoryRepository
 
+    @Singleton
     @Binds
     abstract fun transactionsRepository(repo: TransactionRepositoryImpl): TransactionRepository
 
