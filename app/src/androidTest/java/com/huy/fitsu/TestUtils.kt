83dcd686c76ccd1bb72997022c4f54c7b6d30a45
Huy Bui <huythdnbkltv@gmail.com>
2020-04-13 11:10:32 +0300

    Fix tests for AddEditTransactionFragment

--- app/src/androidTest/java/com/huy/fitsu/TestUtils.kt
+++ app/src/androidTest/java/com/huy/fitsu/TestUtils.kt
@@ -1,7 +1,12 @@
 package com.huy.fitsu
 
+import android.view.View
+import android.view.ViewGroup
 import androidx.paging.PagedList
 import androidx.paging.PositionalDataSource
+import org.hamcrest.Description
+import org.hamcrest.Matcher
+import org.hamcrest.TypeSafeMatcher
 import org.mockito.ArgumentMatchers
 import org.mockito.Mockito
 
@@ -35,4 +40,22 @@ fun <T> MutableList<out T>.toTestPagedList(): PagedList<T> {
         .setFetchExecutor { it.run() }
         .setNotifyExecutor { it.run() }
         .build()
+}
+
+fun childAtPosition(
+    parentMatcher: Matcher<View>, position: Int
+): Matcher<View> {
+
+    return object : TypeSafeMatcher<View>() {
+        override fun describeTo(description: Description) {
+            description.appendText("Child at position $position in parent ")
+            parentMatcher.describeTo(description)
+        }
+
+        public override fun matchesSafely(view: View): Boolean {
+            val parent = view.parent
+            return parent is ViewGroup && parentMatcher.matches(parent)
+                    && view == parent.getChildAt(position)
+        }
+    }
 }
--- app/src/androidTest/java/com/huy/fitsu/addEditTransaction/AddEditTransactionFragmentTest.kt
+++ app/src/androidTest/java/com/huy/fitsu/addEditTransaction/AddEditTransactionFragmentTest.kt
@@ -1,35 +1,34 @@
 package com.huy.fitsu.addEditTransaction
 
-import android.widget.DatePicker
 import androidx.fragment.app.testing.FragmentScenario
 import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.test.core.app.ApplicationProvider
+import androidx.test.espresso.Espresso.onData
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.action.ViewActions.replaceText
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.contrib.PickerActions
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.huy.fitsu.FitsuApplication
 import com.huy.fitsu.R
+import com.huy.fitsu.childAtPosition
 import com.huy.fitsu.data.model.Category
 import com.huy.fitsu.data.model.Transaction
 import com.huy.fitsu.data.repository.CategoryRepository
 import com.huy.fitsu.data.repository.TransactionRepository
 import com.huy.fitsu.util.DateConverter
-import com.huy.fitsu.util.wrapEspressoIdlingResource
 import kotlinx.coroutines.runBlocking
-import org.hamcrest.CoreMatchers.equalTo
+import org.hamcrest.CoreMatchers.allOf
+import org.hamcrest.CoreMatchers.anything
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.Mockito.mock
 import org.mockito.Mockito.verify
-import java.util.*
 
 
 @RunWith(AndroidJUnit4::class)
@@ -50,22 +49,18 @@ class AddEditTransactionFragmentTest {
         transactionRepository = appComponent.transactionRepository
         categoryRepository = appComponent.categoryRepository
 
-        wrapEspressoIdlingResource {
-            runBlocking {
-                categoryRepository.insertNewCategory(testCategory)
-                categoryRepository.insertNewCategory(testCategory2)
-                transactionRepository.insertNewTransaction(testTransaction)
-            }
+        runBlocking {
+            categoryRepository.insertNewCategory(testCategory)
+            categoryRepository.insertNewCategory(testCategory2)
+            transactionRepository.insertNewTransaction(testTransaction)
         }
     }
 
 
     @After
-    fun tearDown() = wrapEspressoIdlingResource {
-        runBlocking {
-            categoryRepository.deleteAllCategories()
-            transactionRepository.deleteAllTransactions()
-        }
+    fun tearDown() = runBlocking {
+        categoryRepository.deleteAllCategories()
+        transactionRepository.deleteAllTransactions()
     }
 
     @Test
@@ -97,39 +92,39 @@ class AddEditTransactionFragmentTest {
 
     @Test
     fun updateTransaction_shouldDisplayOnDashboard() {
-        val dateString = "Friday, 10 April 2020"
-        val testDate = DateConverter.stringToDate(dateString)
-        val calendar = Calendar.getInstance().apply { time = testDate }
-        val testYear = calendar.get(Calendar.YEAR)
-        val testMonth = calendar.get(Calendar.MONTH)
-        val testDay = calendar.get(Calendar.DAY_OF_MONTH)
-        launchFragment()
+        val navController = mock(NavController::class.java)
+        launchFragment().onFragment {
+            Navigation.setViewNavController(it.view!!, navController)
+        }
 
         // Change transaction value
         onView(withId(R.id.transaction_value_edit_text))
             .perform(replaceText("123"))
 
-        // Change transaction date value
-        onView(withId(R.id.transaction_date_button))
-            .perform(click())
-        onView(withClassName(equalTo(DatePicker::class.java.name)))
-            .perform(PickerActions.setDate(testYear, testMonth, testDay))
 
         // Change transaction's category
         onView(withId(R.id.transaction_category_button))
             .perform(click())
-        onView(withText(testCategory2.title))
-            .perform(click())
+        val appCompatCheckedTextView = onData(anything())
+            .inAdapterView(
+                allOf(
+                    withId(R.id.select_dialog_listview),
+                    childAtPosition(
+                        withId(R.id.contentPanel),
+                        1
+                    )
+                )
+            )
+            .atPosition(0)
+        appCompatCheckedTextView.perform(click())
         onView(withId(android.R.id.button1))
             .perform(click())
 
         // Click "Update" fab
-        onView(withId(R.id.add_transaction_fab))
+        onView(withId(R.id.transaction_update_button))
             .perform(click())
 
         onView(withText(testCategory2.title))
-            .check(matches(isDisplayed()))
-        onView(withText(dateString))
             .check(matches(isDisplayed()))
 
     }
