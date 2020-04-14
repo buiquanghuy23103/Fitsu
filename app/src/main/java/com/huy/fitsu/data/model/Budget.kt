ef15cd2156a21234edea185f72aa13baa4057dbc
Huy Bui <huythdnbkltv@gmail.com>
2020-04-14 21:54:51 +0300

    Add Budget model to db

--- app/src/main/java/com/huy/fitsu/data/model/Budget.kt
+++ app/src/main/java/com/huy/fitsu/data/model/Budget.kt
@@ -1,11 +1,15 @@
 package com.huy.fitsu.data.model
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import java.util.*
 
+@Entity(tableName = "budgets")
 data class Budget(
-    val id : String = UUID.randomUUID().toString(),
-    val value : Int = 0,
-    val expense : Int = 0,
-    val createdAt : Date = Date(),
-    val budgetDuration: BudgetDuration = BudgetDuration.WEEKLY
+    @PrimaryKey @ColumnInfo(name = "id") val id : String = UUID.randomUUID().toString(),
+    @ColumnInfo(name = "value") val value : Int = 0,
+    @ColumnInfo(name = "expense") val expense : Int = 0,
+    @ColumnInfo(name = "createdAt") val createdAt : Date = Date(),
+    @ColumnInfo(name = "budget_duration") val budgetDuration: BudgetDuration = BudgetDuration.WEEKLY
 )
