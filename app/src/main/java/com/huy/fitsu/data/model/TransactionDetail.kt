74b9df4c3274afa97f85dc83c40bf3185b458d45
Huy Bui <huythdnbkltv@gmail.com>
2020-04-15 18:46:42 +0300

    Add createdAt field of type LocalDate to TransactionDetail.kt

--- app/src/main/java/com/huy/fitsu/data/model/TransactionDetail.kt
+++ app/src/main/java/com/huy/fitsu/data/model/TransactionDetail.kt
@@ -1,16 +1,21 @@
 package com.huy.fitsu.data.model
 
 import androidx.room.DatabaseView
+import java.time.LocalDate
 import java.util.*
 
-@DatabaseView("SELECT transactions.id, transactions.date, transactions.value, transactions.categoryId," +
-        "categories.title AS categoryTitle, categories.color AS categoryColor " +
-        "FROM transactions INNER JOIN categories " +
-        "ON transactions.categoryId = categories.id")
+@DatabaseView(
+    "SELECT transactions.id, transactions.date, transactions.value, " +
+            "transactions.categoryId, transactions.createdAt, " +
+            "categories.title AS categoryTitle, categories.color AS categoryColor " +
+            "FROM transactions INNER JOIN categories " +
+            "ON transactions.categoryId = categories.id"
+)
 data class TransactionDetail(
     val id: String,
     val value: Int,
     val date: Date,
+    val createdAt: LocalDate,
     val categoryId: String,
     val categoryTitle: String,
     val categoryColor: Int
