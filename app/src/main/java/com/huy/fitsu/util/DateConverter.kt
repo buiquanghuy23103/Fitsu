47ce2dc31440c2017d1088391f115f45464f2d19
Huy Bui <huythdnbkltv@gmail.com>
2020-04-15 21:51:05 +0300

    Update "createdAt" field instead of "date" field on setupDateButton

--- app/src/main/java/com/huy/fitsu/util/DateConverter.kt
+++ app/src/main/java/com/huy/fitsu/util/DateConverter.kt
@@ -1,7 +1,9 @@
 package com.huy.fitsu.util
 
 import java.text.SimpleDateFormat
+import java.time.Instant
 import java.time.LocalDate
+import java.time.ZoneId
 import java.time.format.DateTimeFormatter
 import java.time.format.FormatStyle
 import java.util.*
@@ -12,8 +14,9 @@ object DateConverter {
     private val locale = Locale("en-US")
     private val formatter = SimpleDateFormat(date_format, locale)
     private val dateFormatter = DateTimeFormatter.ofLocalizedDate(FormatStyle.FULL)
+    private val zoneId = ZoneId.systemDefault()
 
-    fun dateToString(date: Date) : String {
+    fun dateToString(date: Date): String {
         return formatter.format(date)
     }
 
@@ -21,10 +24,21 @@ object DateConverter {
         return formatter.parse(string)
     }
 
-    fun localDateToString(localDate: LocalDate?) : String?
-        = localDate?.format(dateFormatter)
+    fun localDateToString(localDate: LocalDate?): String? = localDate?.format(dateFormatter)
 
-    fun stringToLocalDate(string: String?) : LocalDate?
-        = string?.let { LocalDate.parse(it, dateFormatter) }
+    fun stringToLocalDate(string: String?): LocalDate? =
+        string?.let { LocalDate.parse(it, dateFormatter) }
+
+    fun localDateToEpochSeconds(localDate: LocalDate?): Long? =
+        localDate?.atStartOfDay(zoneId)
+            ?.toInstant()
+            ?.toEpochMilli()
+
+    fun epochSecondsToLocalDate(epochSeconds: Long?): LocalDate? =
+        epochSeconds?.let {
+            Instant.ofEpochMilli(it).atZone(
+                zoneId
+            ).toLocalDate()
+        }
 
 }
