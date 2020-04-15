8c392cb90f3ccca47a7c24df63529fa1f5423166
Huy Bui <huythdnbkltv@gmail.com>
2020-04-15 18:41:42 +0300

    Add createdAt field of type LocalDate to Transaction.kt

--- app/src/main/java/com/huy/fitsu/data/model/Transaction.kt
+++ app/src/main/java/com/huy/fitsu/data/model/Transaction.kt
@@ -3,6 +3,7 @@ package com.huy.fitsu.data.model
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import java.time.LocalDate
 import java.util.*
 
 @Entity(tableName = "transactions")
@@ -10,5 +11,6 @@ data class Transaction(
     @PrimaryKey @ColumnInfo(name = "id") val id: String = UUID.randomUUID().toString(),
     @ColumnInfo(name = "value") val value: Int = 0,
     @ColumnInfo(name = "date") val date: Date = Date(),
-    @ColumnInfo(name = "categoryId") val categoryId: String
+    @ColumnInfo(name = "categoryId") val categoryId: String,
+    @ColumnInfo(name = "createdAt") val createdAt: LocalDate = LocalDate.now()
 )
