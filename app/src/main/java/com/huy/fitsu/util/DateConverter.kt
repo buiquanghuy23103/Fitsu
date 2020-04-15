5d8d5eb179a7cdb4bd8c065098b067547905bd9a
Huy Bui <huythdnbkltv@gmail.com>
2020-04-15 18:55:14 +0300

    Add String <-> LocalDate converters

--- app/src/main/java/com/huy/fitsu/util/DateConverter.kt
+++ app/src/main/java/com/huy/fitsu/util/DateConverter.kt
@@ -1,6 +1,9 @@
 package com.huy.fitsu.util
 
 import java.text.SimpleDateFormat
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.time.format.FormatStyle
 import java.util.*
 
 object DateConverter {
@@ -8,6 +11,7 @@ object DateConverter {
     private const val date_format = "EEEE, dd MMMM yyyy"
     private val locale = Locale("en-US")
     private val formatter = SimpleDateFormat(date_format, locale)
+    private val dateFormatter = DateTimeFormatter.ofLocalizedDate(FormatStyle.FULL)
 
     fun dateToString(date: Date) : String {
         return formatter.format(date)
@@ -16,5 +20,11 @@ object DateConverter {
     fun stringToDate(string: String): Date? {
         return formatter.parse(string)
     }
+
+    fun localDateToString(localDate: LocalDate?) : String?
+        = localDate?.format(dateFormatter)
+
+    fun stringToLocalDate(string: String?) : LocalDate?
+        = string?.let { LocalDate.parse(it, dateFormatter) }
 
 }
