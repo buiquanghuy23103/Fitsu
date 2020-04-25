a54f94d06ea84cbd674ed0c61293d6674531d84c
Huy Bui <huythdnbkltv@gmail.com>
2020-04-25 14:05:48 +0300

    Remove color seek bar

--- app/src/main/java/com/huy/fitsu/addEditCategory/AddEditCategoryFragment.kt
+++ app/src/main/java/com/huy/fitsu/addEditCategory/AddEditCategoryFragment.kt
@@ -6,14 +6,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
-import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
-import com.divyanshu.colorseekbar.ColorSeekBar
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.huy.fitsu.FitsuApplication
 import com.huy.fitsu.R
@@ -96,28 +94,17 @@ class AddEditCategoryFragment: Fragment() {
     }
 
     private fun setupColorSelector() {
-        binding.categoryChangeColorButton.setOnClickListener {
-            binding.categoryColorSeekBar.visibility = View.VISIBLE
-        }
-        binding.categoryColorSeekBar.setOnColorChangeListener(
-            object : ColorSeekBar.OnColorChangeListener {
-                override fun onColorChangeListener(color: Int) {
-                    binding.categoryChangeColorButton.setBackgroundColor(color)
-                }
-            }
-        )
+
     }
 
     private fun setupUpdateCategoryButton(category: Category) {
         binding.categoryUpdateButton.setOnClickListener {
             val title = category_title_edit_text.text.toString()
             val budgetDuration = category_budget_duration_edit_text.text.toString()
-            val color = category_color_seek_bar.getColor()
             val newCategory = category.copy(
                 title = title,
                 budgetDuration = BudgetDuration.valueOf(budgetDuration)
             )
-            if (category_color_seek_bar.isVisible) newCategory.color = color
             viewModel.updateCategory(newCategory)
         }
     }
