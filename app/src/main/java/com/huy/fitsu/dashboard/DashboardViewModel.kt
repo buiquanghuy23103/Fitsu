b646545bfc7cc80726d5e29ea5aab410f286fafc
Huy Bui <huythdnbkltv@gmail.com>
2020-05-14 10:12:50 +0300

    Use NestedScrollView for full-screen scrolling

--- app/src/main/java/com/huy/fitsu/dashboard/DashboardViewModel.kt
+++ app/src/main/java/com/huy/fitsu/dashboard/DashboardViewModel.kt
@@ -3,9 +3,7 @@ package com.huy.fitsu.dashboard
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.map
-import androidx.paging.PagedList
-import com.huy.fitsu.data.model.Budget
-import com.huy.fitsu.data.repository.BudgetRepository
+import com.huy.fitsu.data.model.CategoryReport
 import com.huy.fitsu.data.repository.TransactionRepository
 import com.huy.fitsu.util.toCurrencyString
 import javax.inject.Inject
@@ -19,6 +17,9 @@ class DashboardViewModel @Inject constructor(
 
     // Used by data binding
     val accountBalanceString = accountBalance.map { it.toCurrencyString() }
+
+    val transactionCountByCategory: LiveData<List<CategoryReport>> =
+        transactionRepository.transactionCountByCategory()
 
     fun saveAccountBalance(accountBalance: Float) {
         transactionRepository.saveAccountBalance(accountBalance)
